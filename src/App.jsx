4fa43dacb193b5aebdb563f3358b8d3b8df80d83
LilianaFranco <franco0123@gmail.com>
2023-03-21 17:57:18 -0500

    Use Material UI to create three fields and two button for login enrollment

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,68 @@
 import "./App.css";
-import { Box, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from "react";
 
 function App() {
+  //State to show the password with eye icon
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+
   return (
     <div className="App">
-      <Box>
+      <Box
+        sx={{
+          width: "100%",
+          minHeight: "100vh",
+          padding: "10px 20px",
+        }}
+      >
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} sx={{ border: "1px solid black" }}>
-            item 1
+          <Grid item xs={12} sm={6}>
+            <TextField label="Ingresa tu nombre" variant="outlined" fullWidth />
           </Grid>
-          <Grid item xs={12} sm={6} sx={{ border: "1px solid black" }}>
-            item 2
+          <Grid item xs={12} sm={6}>
+            <TextField label="Ingresa tu email" variant="outlined" fullWidth />
           </Grid>
-          <Grid item xs={12} sm={6} sx={{ border: "1px solid black" }}>
-            item 3
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} sx={{ border: "1px solid black" }}>
-            item 4
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
           </Grid>
         </Grid>
+        <Stack spacing={2} sx={{ m: 2 }} direction="row">
+          <Button variant="contained">Ingresar</Button>
+          <Button variant="outlined">Cancelar</Button>
+        </Stack>
       </Box>
     </div>
   );
