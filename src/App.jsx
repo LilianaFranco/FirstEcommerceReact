74265f9d1a1246be84cd9c5216675134bc76fff8
LilianaFranco <franco0123@gmail.com>
2023-03-21 17:10:11 -0500

    Create empty grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Box, Grid } from "@mui/material";
 
 function App() {
-  return <div className="App">Hola, Mundo</div>;
+  return (
+    <div className="App">
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} sx={{ border: "1px solid black" }}>
+            item 1
+          </Grid>
+          <Grid item xs={12} sm={6} sx={{ border: "1px solid black" }}>
+            item 2
+          </Grid>
+          <Grid item xs={12} sm={6} sx={{ border: "1px solid black" }}>
+            item 3
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} sx={{ border: "1px solid black" }}>
+            item 4
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
