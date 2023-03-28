161ec6f87bba6fd576107a7b05446250106ba2c2
LilianaFranco <franco0123@gmail.com>
2023-03-28 14:43:08 -0300

    Product list working with MUI.

--- src/Components/Common/ProductCard.jsx
+++ src/Components/Common/ProductCard.jsx
@@ -1,16 +1,36 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const ProductCard = ({ element }) => {
   console.log(element);
   return (
-    <div style={{ border: "2px solid black" }}>
-      <h2>{element.name}</h2>
-      <h3>{element.price}</h3>
-      <img src={element.img} alt="" style={{ width: "200px" }} />
-      <Link to={`productDetail/${element.id}`}>
-        <button>Ver detalle</button>
-      </Link>
+    <div>
+      <Card sx={{ maxWidth: 345, p: 1, m: 3 }}>
+        <CardMedia
+          component="img"
+          alt=""
+          height="140"
+          image={element.img}
+          margin="2px"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {element.name}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Ver detalle</Button>
+          <Button size="small">Eliminar</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
